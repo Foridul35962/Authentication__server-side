44430e6fa0324d9a62688cc82750023052276cc4
Foridul35962 <foridulislam35962@gmail.com>
2025-10-28 01:25:59 +0600

    index file change for deploy

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ import connectDB from './src/db/database.js'
 
 const PORT = process.env.PORT || 3000
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server is running on http://localhost:${PORT}`);
-    })
-}).catch((err)=>{
-    console.log('server connection failed',err);
-})
+// connectDB().then(()=>{
+//     app.listen(PORT,()=>{
+//         console.log(`server is running on http://localhost:${PORT}`);
+//     })
+// }).catch((err)=>{
+//     console.log('server connection failed',err);
+// })
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+    if (!isConnected) {
+        try {
+            await connectDB()
+            isConnected = true
+            console.log('DB Connected')
+        } catch (err) {
+            console.error('DB Connection Failed', err)
+            return res.status(500).json({ error: 'DB connection failed' })
+        }
+    }
+
+    return app(req, res)
+}
